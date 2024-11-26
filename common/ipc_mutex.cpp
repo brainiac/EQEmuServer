5681adcaef630d7010b79ee6d2aa9f07dce95f22
brainiac <brainiac2k@gmail.com>
2024-11-25 16:18:58 -0800

    Fix naming of win32 mutex

--- common/ipc_mutex.cpp
+++ common/ipc_mutex.cpp
@@ -45,9 +45,9 @@ namespace EQ {
 		imp_ = new Implementation;
 #ifdef _WINDOWS
 		auto Config = EQEmuConfig::get();
-		std::string final_name = fmt::format("{}/EQEmuMutex_{}", Config->SharedMemDir, name);
+		std::string final_name = fmt::format("Global\\EQEmuMutex_{}", name);
 
-		imp_->mut_ = CreateMutex(nullptr,
+		imp_->mut_ = CreateMutexA(nullptr,
 			FALSE,
 			final_name.c_str());
 
