c143b85eb2086590a621f3ea5b215e5a2da5ac37
brainiac <brainiac2k@gmail.com>
2026-04-06 01:18:58 -0700

    WIP

--- common/file.cpp
+++ common/file.cpp
@@ -16,55 +16,104 @@
 	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 #include "file.h"
-
-#include "common/platform/platform.h"
 #include "fmt/format.h"
 
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <sys/stat.h>
 #include <vector>
 
+#if defined(_WIN32)
+#include "common/platform/platform.h"
+#endif
 
 namespace fs = std::filesystem;
 
+constexpr const char* EQEMU_CONFIG_FILENAME = "eqemu_config.json";
+constexpr const char* EQEMU_LOGIN_FILENAME = "login.json";
 
-bool File::Exists(const std::string &name)
+bool File::Exists(const std::string& name)
 {
-	struct stat sb{};
-	if (stat(name.c_str(), &sb) == 0) {
-		return true;
-	}
+	std::error_code ec;
 
-	return false;
+	return fs::exists(name, ec);
 }
 
 void File::Makedir(const std::string &directory_name)
 {
-	try {
+	try
+	{
 		fs::create_directory(directory_name);
 		fs::permissions(directory_name, fs::perms::owner_all);
 	}
-	catch (const fs::filesystem_error &ex) {
-		std::cout << "Failed to create directory: " << directory_name << std::endl;
-		std::cout << ex.what() << std::endl;
+	catch (const fs::filesystem_error& ex)
+	{
+		std::cout << "Failed to create directory: " << directory_name << "\n";
+		std::cout << ex.what() << "\n";
 	}
+}
+
+std::string File::GetCurrentProcessPath()
+{
+	std::error_code ec;
+
+#ifdef __linux__
+	fs::path result = fs::canonical("/proc/self/exe", ec).string();
+	if (ec)
+	{
+		result = fs::current_path(ec).string();
+	}
+#else
+	wchar_t buffer[1024];
+	::GetModuleFileNameW(nullptr, buffer, 1024);
+
+	fs::path exePath(buffer, std::filesystem::path::native_format);
+	return fs::canonical(exePath.parent_path(), ec).string();
+#endif
+}
+
+static fs::path FindConfigDirectory(const fs::path& basePath)
+{
+	std::error_code ec;
+
+	if (fs::is_regular_file(basePath / EQEMU_CONFIG_FILENAME, ec))
+	{
+		return basePath;
+	}
+
+	if (fs::is_regular_file(basePath.parent_path() / EQEMU_CONFIG_FILENAME, ec))
+	{
+		return basePath.parent_path();
+	}
+
+	if (fs::is_regular_file(basePath / EQEMU_LOGIN_FILENAME, ec))
+	{
+		return basePath;
+	}
+
+	if (fs::is_regular_file(basePath.parent_path() / EQEMU_LOGIN_FILENAME, ec))
+	{
+		return basePath.parent_path();
+	}
+
+	return {};
 }
 
 std::string File::FindEqemuConfigPath()
 {
-	if (File::Exists(fs::path{File::GetCwd() + "/eqemu_config.json"}.string())) {
-		return File::GetCwd();
+	std::error_code ec;
+	fs::path currentPath = fs::absolute(fs::current_path(ec)).lexically_normal();
+
+	fs::path configPath = FindConfigDirectory(currentPath);
+
+	if (configPath.empty())
+	{
+		configPath = FindConfigDirectory(GetCurrentProcessPath());
 	}
-	else if (File::Exists(fs::path{File::GetCwd() + "/../eqemu_config.json"}.string())) {
-		return canonical(fs::path{File::GetCwd() + "/../"}).string();
-	}
-	else if (File::Exists(fs::path{File::GetCwd() + "/login.json"}.string())) {
-		return File::GetCwd();
-	}
-	else if (File::Exists(fs::path{File::GetCwd() + "/../login.json"}.string())) {
-		return canonical(fs::path{File::GetCwd() + "/../"}).string();
+
+	if (!configPath.empty())
+	{
+		return fs::canonical(configPath, ec).string();
 	}
 
 	return {};
@@ -75,23 +124,34 @@ std::string File::GetCwd()
 	return fs::current_path().string();
 }
 
-FileContentsResult File::GetContents(const std::string &file_name)
+FileContentsResult File::GetContents(const std::string& file_name)
 {
-	std::ifstream f(file_name, std::ios::in | std::ios::binary);
-	if (!f) {
-		return { .error = fmt::format("Couldn't open file [{}]", file_name) };
+	try {
+		std::ifstream f(file_name, std::ios::in | std::ios::binary);
+		if (!f)
+		{
+			return {
+				.error = fmt::format("Couldn't open file [{}]", file_name)
+			};
+		}
+
+		constexpr size_t CHUNK_SIZE = 4096;  // Read 4KB chunks
+		std::string lines;
+		std::vector<char> buffer(CHUNK_SIZE);
+
+		while (f.read(buffer.data(), CHUNK_SIZE) || f.gcount() > 0)
+		{
+			lines.append(buffer.data(), f.gcount());
+		}
+
+		return FileContentsResult{
+			.contents = lines
+		};
 	}
-
-	constexpr size_t CHUNK_SIZE = 4096;  // Read 4KB chunks
-	std::string lines;
-	std::vector<char> buffer(CHUNK_SIZE);
-
-	while (f.read(buffer.data(), CHUNK_SIZE) || f.gcount() > 0) {
-		lines.append(buffer.data(), f.gcount());
+	catch (const std::exception& ex)
+	{
+		return {
+			.error = fmt::format("Error reading file [{}]: {}", file_name, ex.what())
+		};
 	}
-
-	return FileContentsResult{
-		.contents = lines,
-		.error = {}
-	};
 }
